588c1ac39d8f6f14a3cb886a649dd605b773e4c0
James Woody <woody.james@gmail.com>
2015-01-30 12:12:36 -0600

    I have commented out the unused example code I have added a variable for CC# I belive I have MIDI.sendControlChange worked out

--- workingExample.ino
+++ workingExample.ino
@@ -1,3 +1,5 @@
+
+
 #include <Button.h>
 #include <midi_Settings.h>
 #include <midi_Message.h>
@@ -10,9 +12,12 @@ This sketch is my base working model.  Code compiles.  I've added an
 addition menu item of "Set Volume", the value of pedal one is read,
 mapped 0 to 100, constrained then printed to LCD.
 
-Current issue is figuring out how to make
-	MIDI.sendControlChange();
-to work.
+I have commented out the example menu items, leaving on the items
+I have created.  Will delete them in next update if all works
+
+created byte midiCCtoSend, in the menu, user will select which CC#
+to send to, then in "Set Volume" the peadal value will be sent
+to both the LCD screen and out on the midiCCtoSend
 
 
  
@@ -72,6 +77,7 @@ byte sel = 0;
 unsigned int bar = 1;
 long baz  = 0;
 float bak = 0.0;
+byte midiCCtoSend = 0;    //initiailize variable for MIDI.sendControlChange(midiCCtoSend, pedalValue, 1)
 
   // Create a list of states and values for a select input
 MENU_SELECT_ITEM  sel_ign = { 2, {"Ignore"} };
@@ -88,23 +94,33 @@ MENU_SELECT state_select = { &sel,           MENU_SELECT_SIZE(state_list),   MEN
   // values to use 
 
                     //    TYPE            MAX    MIN    TARGET 
+/*Comment out, but don't delete yet.
 MENU_VALUE foo_value = { TYPE_BYTE,       100,   0,     MENU_TARGET(&foo) };
 MENU_VALUE bar_value = { TYPE_UINT,       10000, 100,   MENU_TARGET(&bar) };
 MENU_VALUE baz_value = { TYPE_LONG,       10000, 1,     MENU_TARGET(&baz) };
 MENU_VALUE bak_value = { TYPE_FLOAT_1000, 0,     0,     MENU_TARGET(&bak) };
 MENU_VALUE sel_value = { TYPE_SELECT,     0,     0,     MENU_TARGET(&state_select) };
+*/
+MENU_VALUE midiCCtoSend_value = { TYPE_BYTE, 127, 0, MENU_TARGET(&midiCCtoSend) };
 
                     //        LABEL           TYPE        LENGTH    TARGET
+/*Comment out, but don't delete yet.
 MENU_ITEM item_checkme  = { {"Byte Edit"},    ITEM_VALUE,  0,        MENU_TARGET(&foo_value) };
 MENU_ITEM item_barme    = { {"UInt Edit"},     ITEM_VALUE,  0,        MENU_TARGET(&bar_value) };
 MENU_ITEM item_bazme    = { {"Long Edit"},    ITEM_VALUE,  0,        MENU_TARGET(&baz_value) };
 MENU_ITEM item_bakme    = { {"Float Edit"},   ITEM_VALUE,  0,        MENU_TARGET(&bak_value) };
 MENU_ITEM item_state    = { {"Select Input"}, ITEM_VALUE,  0,        MENU_TARGET(&sel_value) };
+*/
 MENU_ITEM item_testme   = { {"Test Action"},  ITEM_ACTION, 0,        MENU_TARGET(uiQwkScreen) };
 MENU_ITEM item_voltest  =  {  {"Set Volume"}, ITEM_ACTION, 0,       MENU_TARGET(uiSetVolume) };
+MENU_ITEM item_midiCCtoSend = {  {"Choose CC#"}, ITEM_VALUE, 0,  MENU_TARGET(&midiCCtoSend) };
 
                    //        List of items in menu level
-MENU_LIST const root_list[]   = { &item_checkme, &item_barme, &item_bazme, &item_bakme, &item_state, &item_testme, &item_voltest };
+/*Comment out, but don't delete yet.
+MENU_LIST const root_list[]   = { &item_checkme, &item_barme, &item_bazme, &item_bakme, &item_state, &item_testme, &item_voltest, &item_midiCCtoSend };
+*/
+MENU_LIST const root_list[]   = { &item_testme, &item_voltest, &item_midiCCtoSend };
+
 
                   // Root item is always created last, so we can add all other items to it
 MENU_ITEM menu_root     = { {"Root"},        ITEM_MENU,   MENU_SIZE(root_list),    MENU_TARGET(&root_list) };
@@ -115,9 +131,14 @@ LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
 
 OMMenuMgr Menu(&menu_root);
 
+//create an instance of MIDI - also need to call it in "SETUP"
+MIDI_CREATE_DEFAULT_INSTANCE();
+
 
 
 void setup() {
+  
+  MIDI.begin();
   
   //ini pinMode for pedalOnePin
   pinMode(A2, INPUT);
@@ -194,7 +215,7 @@ void uiSetVolume(){
       lcd.clear();
       lcd.setCursor(0,2);
       lcd.print(myVar);
-  //    MIDI.sendControlChange(10, 10, 1);  //need to figure this one out
+      MIDI.sendControlChange(midiCCtoSend, myVar, 1);
       delay(2);
     ; // wait!
   }
